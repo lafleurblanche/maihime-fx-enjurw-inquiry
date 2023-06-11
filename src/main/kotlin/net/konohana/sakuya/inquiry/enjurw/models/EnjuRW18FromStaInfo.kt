d0a93ce19d991c6dab2c82faed5d4b2a3b958f6b
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-06-11 20:59:58 +0900

    20230611 新規作成

---
+++ src/main/kotlin/net/konohana/sakuya/inquiry/enjurw/models/EnjuRW18FromStaInfo.kt
@@ -0,0 +1,17 @@
+package net.konohana.sakuya.inquiry.enjurw.models
+
+import org.jetbrains.exposed.sql.Table
+
+/**
+ * ## EnjuRW18FromStaInfoモデル
+ * * 卯塚線乗車駅情報
+ * @author lafleurblanche
+ */
+object EnjuRW18FromStaInfo : Table("enju_rw18_from_sta_info") {
+    /** ID(自動採番) */
+    val id = integer("id").autoIncrement()
+    /** 乗車駅コード */
+    val fromStaCode = varchar("from_sta_code", length = 20)
+    /** 乗車駅名 */
+    val fromStaName = varchar("from_sta_name", length = 20)
+}
