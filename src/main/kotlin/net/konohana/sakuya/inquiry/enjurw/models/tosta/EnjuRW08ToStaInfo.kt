8ee89169e5340a0ecf055d008d40e97018698131
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-06-13 18:25:57 +0900

    20230613 新規作成

---
+++ src/main/kotlin/net/konohana/sakuya/inquiry/enjurw/models/tosta/EnjuRW08ToStaInfo.kt
@@ -0,0 +1,17 @@
+package net.konohana.sakuya.inquiry.enjurw.models.tosta
+
+import org.jetbrains.exposed.sql.Table
+
+/**
+ * ## EnjuRW08ToStaInfoモデル
+ * * 笒谷線降車駅情報
+ * @author lafleurblanche
+ */
+object EnjuRW08ToStaInfo : Table("enju_rw08_to_sta_info") {
+    /** ID(自動採番) */
+    val id = integer("id").autoIncrement()
+    /** 降車駅コード */
+    val toStaCode = varchar("to_sta_code", length = 20)
+    /** 降車駅名 */
+    val toStaName = varchar("to_sta_name", length = 20)
+}
