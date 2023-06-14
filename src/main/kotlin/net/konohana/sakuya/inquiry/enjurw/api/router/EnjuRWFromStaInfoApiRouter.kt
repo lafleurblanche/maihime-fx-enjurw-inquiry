ee4cc8f39005abfb4f9d66fe2ee8b89e3aab015d
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-06-14 23:10:45 +0900

    20230614 Add Route

--- src/main/kotlin/net/konohana/sakuya/inquiry/enjurw/api/router/EnjuRWFromStaInfoApiRouter.kt
+++ src/main/kotlin/net/konohana/sakuya/inquiry/enjurw/api/router/EnjuRWFromStaInfoApiRouter.kt
@@ -2,7 +2,11 @@ package net.konohana.sakuya.inquiry.enjurw.api.router
 
 import io.ktor.server.routing.Route
 import net.konohana.sakuya.inquiry.enjurw.api.controller.enjuRW01FromStaInfoController
+import net.konohana.sakuya.inquiry.enjurw.api.controller.enjuRW02FromStaInfoController
+import net.konohana.sakuya.inquiry.enjurw.api.controller.enjuRW03FromStaInfoController
 
 fun Route.enjuRWFromStaInfoApiRouter() {
     enjuRW01FromStaInfoController()
+    enjuRW02FromStaInfoController()
+    enjuRW03FromStaInfoController()
 }
