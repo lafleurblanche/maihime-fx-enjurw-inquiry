9beff493cf7c45083f174c5199229e1de4db6ef7
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-06-16 08:24:00 +0900

    20230616 新規作成

---
+++ src/main/kotlin/net/konohana/sakuya/inquiry/enjurw/api/controller/fromsta/EnjuRW14FromStaInfoController.kt
@@ -0,0 +1,35 @@
+package net.konohana.sakuya.inquiry.enjurw.api.controller.fromsta
+
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.application.call
+import io.ktor.server.response.respond
+import io.ktor.server.routing.Route
+import io.ktor.server.routing.get
+import io.ktor.server.routing.route
+import net.konohana.sakuya.inquiry.enjurw.api.services.fromsta.EnjuRW14FromStaInfoService
+import net.konohana.sakuya.inquiry.enjurw.models.fromsta.dto.EnjuRW14FromStaInfoData
+
+fun Route.enjuRW14FromStaInfoController() {
+    route("api") {
+        route("fromsta") {
+            route("enjurw14") {
+                get("{fromStaCode}") {
+                    val fromStaCode = call.parameters["fromStaCode"] ?: throw IllegalArgumentException("Invalid fromStaCode")
+                    val enjuRW14FromStaInfoData = EnjuRW14FromStaInfoService.findByFromStaCode(fromStaCode)
+                    if (enjuRW14FromStaInfoData != null) {
+                        call.respond(HttpStatusCode.OK, enjuRW14FromStaInfoData)
+                    } else {
+                        call.respond(
+                            HttpStatusCode.OK,
+                            EnjuRW14FromStaInfoData(
+                                id = 0,
+                                fromStaCode = "FRRW9999",
+                                fromStaName = "該当駅なし",
+                            )
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
