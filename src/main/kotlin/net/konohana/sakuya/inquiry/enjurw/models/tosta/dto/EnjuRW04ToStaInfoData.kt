2f6cf334ed3d77d41860ea2cc4e6d1e09734a4cf
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-06-16 10:04:19 +0900

    20230616 Serializable設定

--- src/main/kotlin/net/konohana/sakuya/inquiry/enjurw/models/tosta/dto/EnjuRW04ToStaInfoData.kt
+++ src/main/kotlin/net/konohana/sakuya/inquiry/enjurw/models/tosta/dto/EnjuRW04ToStaInfoData.kt
@@ -1,5 +1,8 @@
 package net.konohana.sakuya.inquiry.enjurw.models.tosta.dto
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class EnjuRW04ToStaInfoData(
     val id: Int,
     val toStaCode: String,
